8587498a55f9f5d6804ddaed67164ce4fc15fb88
tonixhaker <tonixhaker@gmail.com>
2019-03-02 14:27:45 +0200

    Initial commit

--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,23 @@
 const { Schema, model } = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
+
 
 const userSchema = new Schema({
-    firstName: String,
-    lastName: String,
+    firstName: {
+        type:String,
+        required: true,
+    },
+    lastName: {
+        type:String,
+        required: true,
+    },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     phoneNumber: String
 });
 
+userSchema.plugin(uniqueValidator);
 module.exports = model('user', userSchema);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,15 @@
 import express from 'express'
 let router = express.Router();
+const userSchema = require('../models/user');
+
 
 router.get('/user', function(req, res) {
     res.send('user');
+});
+
+router.get('/test', function (req,res) {
+    var user = new userSchema({ email: 'qweeee@qwe.com' });
+    res.send(user.save());
 });
 
 
